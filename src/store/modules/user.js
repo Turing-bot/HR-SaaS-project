b80ee0900dc090ac0cfa50cd5548b722bb041014
C137-R <6199599@gmail.com>
2022-04-20 20:26:51 +0800

    封装Vuex登录Action并处理token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,31 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user '
+
+const state = {
+  token: getToken() // 设置token初始状态
+}
+const mutations = {
+  // 设置token
+  setToken (state, token) {
+    state.token = token
+    setToken(token)
+  },
+  removeToken (state) {
+    state.token = null
+    removeToken()
+  }
+}
+const actions = {
+  async login (context, data) {
+    const result = await login(data)
+    if (result.data.success) {
+      context.commit('setToken', result.data.data)
+    }
+  }
+}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
