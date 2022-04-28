2cafe54ba4fa2699ca88d42184a5917d318a5e09
C137-R <6199599@gmail.com>
2022-04-28 10:26:33 +0800

    封装删除接口，注册下拉菜单事件

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   //it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
     'vue/max-attributes-per-line': [
-      2,
+      1,
       {
         singleline: 10,
         multiline: {
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -5,13 +5,17 @@
       <el-card class="tree-card">
         <!-- 用了一个行列布局 -->
         <!-- 缺少treeNode -->
-        <tree-tools :tree-node="company" />
+        <tree-tools :tree-node="company" :is-root="true" />
         <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
         <el-tree :data="departs" :props="defaultProps" default-expand-all>
           <!-- 说明el-tree里面的这个内容 就是插槽内容 => 填坑内容  => 有多少个节点循环多少次 -->
           <!-- scope-scope 是 tree组件传给每个节点的插槽的内容的数据 -->
           <!-- 顺序一定是 执行slot-scope的赋值 才去执行 props的传值 -->
-          <tree-tools slot-scope="{ data }" :tree-node="data" :is-root="true" />
+          <tree-tools
+            slot-scope="{ data }"
+            :tree-node="data"
+            @delDepts="getDepartments"
+          />
         </el-tree>
       </el-card>
     </div>
@@ -44,7 +48,6 @@ export default {
       this.company = { name: result.companyName, manager: '负责人' }
       // 这里定义一个空串  因为 它是根 所有的子节点的数据pid 都是 ""
       this.departs = tranListToTreeData(result.depts, '')
-      console.log(result)
     }
   }
 }
